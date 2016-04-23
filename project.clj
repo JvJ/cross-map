fe25946a5555d9dcfa841ce0ad855f8ea8e3af70
JvJ <kfjwheeler@gmail.com>
2016-04-23 12:42:20 -0700

    first commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.jvj/cross-map "0.1.0-SNAPSHOT"
+  :description "Two-dimensional map with efficient cross-section capabilities."
+  :url "https://github.com/JvJ/cross-map"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.228"]])
